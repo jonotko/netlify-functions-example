857978b533c56f6640b1bcad30960ac95c299f47
jonotko <j.mastermind1010@gmail.com>
2020-05-29 14:50:56 -0400

    Headers

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,9 @@
 exports.handler = function(event, context, callback) {
   callback(null, {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': 'https://confident-bassi-9d4bb8.netlify.app/'
+    },
     body: JSON.stringify(event)
   });
 };
