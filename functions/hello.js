221854242305f8bb1eb591b2dd0359cac5eaad61
jonotko <j.mastermind1010@gmail.com>
2020-05-29 14:55:00 -0400

    Headers Again

--- functions/hello.js
+++ functions/hello.js
@@ -2,7 +2,7 @@ exports.handler = function(event, context, callback) {
   callback(null, {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': 'https://confident-bassi-9d4bb8.netlify.app/'
+      'AccessControlAllowOrigin': 'https://confident-bassi-9d4bb8.netlify.app'
     },
     body: JSON.stringify(event)
   });
